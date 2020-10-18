67b45e4b2c1c372ce52b6fc0755d534e473ffe7e
Zhigang Wang <zhigangw@hotmail.com>
2020-10-18 11:04:30 -0700

    update app.py to basic and working one

--- server/app.py
+++ server/app.py
@@ -1,22 +1,7 @@
-from flask_cors import CORS
-
-
-# configuration
-DEBUG = True
-
-# instantiate the app
+from flask import Flask
 app = Flask(__name__)
-app.config.from_object(__name__)
-
-# enable CORS
-CORS(app, resources={r'/*': {'origins': '*'}})
 
 
-# sanity check route
-@app.route('/ping', methods=['GET'])
-def ping_pong():
-    return jsonify('pong!')
-
-
-if __name__ == '__main__':
-    app.run()
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
