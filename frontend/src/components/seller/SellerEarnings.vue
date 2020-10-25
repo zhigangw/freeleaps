42676c19d71102664ba9a97375f3f05dd206489c
Zhigang Wang <zhigangw@hotmail.com>
2020-10-25 10:11:57 -0700

    add all pages to components in app.vue

---
+++ frontend/src/components/seller/SellerEarnings.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>SellerEarning</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SellerEarning",
+  props: {},
+
+  data() {
+    return {};
+  },
+
+  created() {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
