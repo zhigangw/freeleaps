3025fbea9c78f94ea59326bf6577a352ccb45457
Zhigang Wang <zhigangw@hotmail.com>
2020-10-25 10:11:42 -0700

    add all pages to components in app.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,83 @@
 <template>
   <div>
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld />
+    <FrontDoor />
+    <UserBasic />
+    <UserLinked />
+    <UserRegister />
+    <UserSignin />
+    <BuyerDashboard/>
+    <BuyerInviteSeller/>
+    <BuyerPackage/>
+    <BuyerProjectList/>
+    <BuyerQuoteView/>
+    <PostProjectDescription/>
+    <PostProjectNote/>
+    <PostProjectReview/>
+    <SellerBuyerRequestView/>
+    <SellerDashboard/>
+    <SellerEarnings/>
+    <SellerMarketplace/>
+    <SellerPackage/>
+    <SellerProjectView/>
+    <SellerQuoting/>
+    <SellerServiceCreate/>
+    <SellerSkill/>
+    <SellerTransferMoney/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import FrontDoor from "./components/user/FrontDoor";
+import UserBasic from "./components/user/UserBasic";
+import UserLinked from "./components/user/UserLinkedin";
+import UserRegister from "./components/user/UserRegister";
+import UserSignin from "./components/user/UserSignin";
+import BuyerDashboard from "./components/buyer/BuyerDashboard";
+import BuyerInviteSeller from "./components/buyer/BuyerInviteSeller";
+import BuyerPackage from "./components/buyer/BuyerPackage";
+import BuyerProjectList from "./components/buyer/BuyerProjectList";
+import BuyerQuoteView from "./components/buyer/BuyerQuoteView";
+import PostProjectDescription from "./components/buyer/PostProjectDescription";
+import PostProjectNote from "./components/buyer/PostProjectNote";
+import PostProjectReview from "./components/buyer/PostProjectReview";
+import SellerBuyerRequestView from "./components/seller/SellerBuyerRequestView";
+import SellerDashboard from "./components/seller/SellerDashboard";
+import SellerEarnings from "./components/seller/SellerEarnings";
+import SellerMarketplace from "./components/seller/SellerMarketplace";
+import SellerPackage from "./components/seller/SellerPackage";
+import SellerProjectView from "./components/seller/SellerProjectView";
+import SellerQuoting from "./components/seller/SellerQuoting";
+import SellerServiceCreate from "./components/seller/SellerServiceCreate";
+import SellerSkill from "./components/seller/SellerSkill";
+import SellerTransferMoney from "./components/seller/SellerTransferMoney";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    FrontDoor,
+    UserBasic,
+    UserLinked,
+    UserRegister,
+    UserSignin,
+    BuyerDashboard,
+    BuyerInviteSeller,
+    BuyerPackage,
+    BuyerProjectList,
+    BuyerQuoteView,
+    PostProjectDescription,
+    PostProjectNote,
+    PostProjectReview,
+    SellerBuyerRequestView,
+    SellerDashboard,
+    SellerEarnings,
+    SellerMarketplace,
+    SellerPackage,
+    SellerProjectView,
+    SellerQuoting,
+    SellerServiceCreate,
+    SellerSkill,
+    SellerTransferMoney
   },
 };
 </script>
