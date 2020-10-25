c90e4ac3d1d082b30ca550bd75f6fdcfa2a7e347
Zhigang Wang <zhigangw@hotmail.com>
2020-10-24 19:53:29 -0700

    new vue files

---
+++ frontend/src/components/Buyer/BuyerQuoteView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>PostQuoteView</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PostQuoteView",
+  props: {},
+
+  data() {
+    return {};
+  },
+
+  created() {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
---
+++ frontend/src/components/Seller/SellerQuoting.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>SellerQuoting</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SellerQuoting",
+  props: {},
+
+  data() {
+    return {};
+  },
+
+  created() {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
