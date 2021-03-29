f2b0c6e726af315bbf540aef717ec9ad04e8be12
Zhigang Wang <zhigangw@hotmail.com>
2021-03-28 23:41:33 -0700

    post request note 1/n

---
+++ frontend/src/components/accordions/templates/BootStrapAccordionItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="accordion-item">
+    <h2 class="accordion-header" :id="addId">
+      <button
+        class="accordion-button"
+        type="button"
+        data-bs-toggle="collapse"
+        :data-bs-target="'#'+collapseId"
+        aria-expanded="false"
+        :aria-controls="collapseId"
+      >
+        <slot name="header" />
+      </button>
+    </h2>
+    <div
+      :id="collapseId"
+      class="accordion-collapse collapse"
+      :aria-labelledby="addId"
+      data-bs-parent="#accordion-parent"
+    >
+      <div class="accordion-body">
+        <slot name="body" />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+let _uid = 0;
+export default {
+  name: "BootStrapAccordionItem",
+  data() {
+    return {
+      addId: null,
+      collapseId: null,
+    };
+  },
+  mounted() {
+    this.addId = "add" + (++_uid).toString();
+    this.collapseId = "collapse" + (++_uid).toString();
+  },
+  components: {},
+  computed: {},
+  methods: {},
+};
+</script>
--- frontend/src/pages/request/PostRequestNote.vue
+++ frontend/src/pages/request/PostRequestNote.vue
@@ -1,37 +1,108 @@
 <template>
-  <div>
-    <h1>PostProjectNote</h1>
+  <div class="lf-board-container">
     <form @submit.prevent="submitForm">
-      <div class="form-control">
-        <label for="total-budget">Total Budget</label>
-        <input type="number" id="total-budget" v-model.trim="notes.totalBudget" />
+      <div class="lf-head-container">
+        <div class="lf-head-left">
+          <h3 class="lf-head-title">Package</h3>
+          <h6 class="lf-head-subtitle">Step 2 of 3</h6>
+        </div>
+        <div class="lf-head-right">
+          <p class="lf-head-notes">
+            - More budget will bring more attractions from talents.
+            <br />- More escorted deposit will be more security to the freelancers.
+          </p>
+        </div>
       </div>
-      <div class="form-control">
-        <label for="escorted-deposit">Escorted Deposit</label>
-        <input type="number" id="escorted-deposit" v-model.trim="notes.escortedDeposit" />
+      <div class="lf-body-container">
+        <div class="lf-body-content">
+          <div class="lf-body-block-container">
+            <h4 class="lf-body-block-container-title">Payment</h4>
+            <div class="lf-body-block-container-body">
+              <div class="lf-body-item-block">
+                <h5 class="lf-body-item-block-label" for="total-budget">Total Budget (USD)</h5>
+                <p class="lf-body-item-block-notes">The total payment upon completion</p>
+                <input
+                  class="lf-body-item--block-input"
+                  type="number"
+                  id="total-budget"
+                  v-model.trim="notes.totalBudget"
+                />
+              </div>
+              <div class="lf-body-item-block">
+                <h5 class="lf-body-item-block-label" for="escorted-deposit">Escorted Deposit (USD)</h5>
+                <p class="lf-body-item-block-notes">The amount escorted by the platform</p>
+                <input
+                  class="lf-body-item--block-input"
+                  type="number"
+                  id="escorted-deposit"
+                  v-model.trim="notes.escortedDeposit"
+                />
+              </div>
+            </div>
+          </div>
+          <div class="lf-body-block-container">
+            <h4 class="lf-body-block-container-title">Plan</h4>
+            <div class="accordion" id="accordion-parent">
+              <boot-strap-accordion-item>
+                <template #header>
+                  <input id="one-time-radio" type="radio" value="one-time" v-model="plan" />
+                  <label class="w-100" for="one-time-radio">One time</label>
+                </template>
+                <template #body>
+                  <p>A paragraph for the main content.</p>
+                  <p>And another one.</p>
+                </template>
+              </boot-strap-accordion-item>
+              <boot-strap-accordion-item>
+                <template #header>
+                  <input id="periodic-radio" type="radio" value="periodic" v-model="plan" />
+                  <label class="w-100" for="periodic-radio">Periodic</label>
+                </template>
+                <template #body>
+                  <p>A paragraph for the main content.</p>
+                  <p>And another one.</p>
+                </template>
+              </boot-strap-accordion-item>
+              <boot-strap-accordion-item>
+                <template #header>
+                  <input id="free-form-radio" type="radio" value="free-form" v-model="plan" />
+                  <label class="w-100" for="free-form-radio">Free form</label>
+                </template>
+                <template #body>
+                  <p>A paragraph for the main content.</p>
+                  <p>And another one.</p>
+                </template>
+              </boot-strap-accordion-item>
+              <div class="lf-body-item-block"></div>
+            </div>
+          </div>
+          <div class="form-control">
+            <label for="estimated-time">Estimated Time</label>
+            <input type="number" id="estimated-time" v-model.trim="notes.estimatedHours" />
+          </div>
+          <div class="form-control">
+            <label for="estimated-time">Estimated Start Date</label>
+            <input type="date" id="estimated-start-date" v-model.trim="notes.startDate" />
+          </div>
+          <div class="form-control">
+            <label for="estimated-time">Estimated Deliver Date</label>
+            <input type="date" id="estimated-deliver-date" v-model.trim="notes.deliverDate" />
+          </div>
+          <div class="form-control">
+            <label for="qualification">Qualification</label>
+            <input type="text" id="qualification" v-model.trim="notes.qualification" />
+          </div>
+          <div class="form-control">
+            <label for="notes">Notes</label>
+            <input type="text" id="notes" v-model.trim="notes.notes" />
+          </div>
+        </div>
       </div>
-      <div class="form-control">
-        <label for="estimated-time">Estimated Time</label>
-        <input type="number" id="estimated-time" v-model.trim="notes.estimatedHours" />
+      <p v-show="hasError()" class="if-errorInput">{{errorMessage}}</p>
+      <div class="lf-submit-container">
+        <button class="if-cancel" type="button" @click="goBack">Back</button>
+        <button class="if-submit" type="submit">Next</button>
       </div>
-      <div class="form-control">
-        <label for="estimated-time">Estimated Start Date</label>
-        <input type="date" id="estimated-start-date" v-model.trim="notes.startDate" />
-      </div>
-      <div class="form-control">
-        <label for="estimated-time">Estimated Deliver Date</label>
-        <input type="date" id="estimated-deliver-date" v-model.trim="notes.deliverDate" />
-      </div>
-      <div class="form-control">
-        <label for="qualification">Qualification</label>
-        <input type="text" id="qualification" v-model.trim="notes.qualification" />
-      </div>
-      <div class="form-control">
-        <label for="notes">Notes</label>
-        <input type="text" id="notes" v-model.trim="notes.notes" />
-      </div>
-      <button type="button" @click="goBack">Back</button>
-      <button type="submit">Next</button>
     </form>
   </div>
 </template>
@@ -39,17 +110,17 @@
 <script>
 import { RequestPostApi, requestPostUtils } from "../../utils/index";
 import { requestPostSkeleton } from "../../types/index";
-
+import BootStrapAccordionItem from "../../components/accordions/templates/BootStrapAccordionItem";
 export default {
   name: "PostRequestNote",
-  props: {
-    requestId: null,
-  },
-
+  props: {},
+  components: { BootStrapAccordionItem },
   data() {
     return {
-      localRequestId: null,
+      plan: null,
+      requestId: null,
       notes: requestPostSkeleton.notes,
+      errorMessage: null,
     };
   },
 
@@ -59,6 +130,9 @@ export default {
     this.fetchLocalStoredNotes();
   },
   methods: {
+    hasError() {
+      return this.errorMessage;
+    },
     gotoNext() {
       this.mnx_navToPostRequestReview(this.localRequestId);
     },
@@ -77,7 +151,6 @@ export default {
           this.notes = notes;
         }
       }
-      console.log(this.notes);
     },
 
     async fetchNotes() {
